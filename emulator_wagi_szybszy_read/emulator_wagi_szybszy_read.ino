b21d8bbd414729d110e93c8ae24cf631d9458b75
MarcinanBarbarzynca <49427297+MarcinanBarbarzynca@users.noreply.github.com>
2021-09-04 17:40:52 +0200

    Emulator który szybciej drukuje odpowiedzi.
    Trzeba sprawdzić czy emulator pokrywa się z prawdziwym działaniem wagi jeszcze. Jeżeli tak to debudowanie interfejsów będzie łątwiejsze

---
+++ emulator_wagi_szybszy_read/emulator_wagi_szybszy_read.ino
@@ -0,0 +1,57 @@
+//ten program emuluje wagę
+//Po wpisaniu C1 & @CRLF to
+//Po Wpisaniu NL & @CRLF to waga wypisuje swój identyfikator
+//
+const unsigned long interval = 1000;
+unsigned long previousMillis = 0;
+
+bool nadawanie_non_stop = true;
+void print_random() {
+  Serial.print("SI : "+String(random(0, 10000)) + "\r\n"); //Wypisuje randomową wartość
+}
+
+void setup() {
+  Serial.setTimeout(200);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  if (nadawanie_non_stop == true) {
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval)
+    {
+      print_random();
+      previousMillis = currentMillis;
+    }
+  }
+  if (Serial.available() > 0) {
+    //char[100] buffer = ""
+    //while Serial.read()!="\n"{
+    //  String wiadomosc = Serial.read();
+    //}
+    String wiadomosc = Serial.readStringUntil('\n');
+    
+    //Serial.println("WIADOMOSC: "+wiadomosc);
+    if (wiadomosc == "C1\r") {
+      Serial.print("C1 A\r\n");
+      nadawanie_non_stop = true;
+    }
+    if (wiadomosc == "C0\r") {
+      Serial.print("C0 A\r\n");
+      nadawanie_non_stop = false;
+    }
+    if (wiadomosc == "NB\r") {
+      Serial.print("NB A"); 
+      Serial.print(" \"999999\"");
+      Serial.print("\r\n");
+     // Serial.print("NB A 999999\r\n");
+    }
+    if (wiadomosc == "?") {
+      Serial.println("Komendy: C0@CRLF; C1@CRLF;  NB@CRLF");
+    }
+    if (wiadomosc == "?\r\n") {
+      Serial.println("Komendy: C0@CRLF; C1@CRLF;  NB@CRLF");
+    }
+  }
+}
